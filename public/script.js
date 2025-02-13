361f1d653fcee42885eb8aa9bb0c0e5e43ffcfba
yada2x <lawrenceltnguyen@gmail.com>
2025-02-13 19:55:43 +1100

    Add basic HTML and JS script

---
+++ public/script.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("prompt_input");
+const button = document.getElementById("submit_button");
+const output = document.getElementById("prompt_output");
+
+console.log("Yay!");
